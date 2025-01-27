2c1a8fc12fe54b716bd0dad9461cb068a0fff629
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-01-27 20:03:24 +0600

    add all file

---
+++ curd_project2/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $conn = mysqli_connect("localhost", "root", "", "cmbd263");
+
+    if(!$conn){
+        echo "Database connection fail";
+    } else {
+        echo "Database connection sucessfull";
+    }
+
+
+
