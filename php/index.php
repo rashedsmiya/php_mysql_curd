ce5400e19dd7f34df109a09fea89f15975113f5f
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-03-30 18:41:00 +0600

    add

--- php/index.php
+++ php/index.php
@@ -1,9 +1,36 @@
 <?php 
 
+//float
 
-     echo "Hello World";
-     echo "This is my first php program";
-     echo "This is my second php program";
+//      $price = 5.000;
+//
+//      var_dump($price);
 
 
- 
+// true & false
+//True = 1;
+// False = 0;
+
+//Boolean
+//$hadi_bad_boy = true;
+//$hadi_bad_boy = false;
+//var_dump($hadi_bad_boy);
+
+// array
+
+//$cell = "01774347837483";
+//$name = "hadijaman";
+//$address = "Dhaka-Mirpur";
+//
+//$person_info = array(
+//    "name" => $name,
+//    "address" => $address,
+//    "cell" => $cell
+//);
+//
+//var_dump($person_info);
+
+//$null = null;
+//$name = $null;
+
+
