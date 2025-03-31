dc84a7f51760830ca4d8b2e1cb623a3c9acfcbeb
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-03-31 17:08:38 +0600

    add

--- index.php
+++ index.php
@@ -246,4 +246,23 @@
 <!--</form>-->
 
 
+<?php //
+//
+//     if($_SERVER["REQUEST_METHOD"] == "POST"){
+//         $num = $_POST['number'];
+//
+//         if($num % 2 == 0){
+//             echo "$num is an Even number";
+//         } else{
+//             echo "$num is an odd number";
+//         }
+//     }
+//?>
+<!---->
+<!--<form method="post">-->
+<!--      Enter a number: <input type="number" name="number">-->
+<!--      <button type="submit">Check</button>-->
+<!--</form>-->
+<!--    -->
+
 
